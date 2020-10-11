6bb5d5f0a1ce47dbdb4060cc692d99cebf1fb246
Alexander Varney <alex@varney.me>
2020-10-11 19:32:35 -0400

    add cors support to server

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bcrypt = require('bcryptjs');
-var mongoose = require('mongoose');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bcrypt = require("bcryptjs");
+var mongoose = require("mongoose");
+var cors = require("cors");
 
-var User = require('./models/User');
+var User = require("./models/User");
 
 mongoose
   .connect(process.env.MONGO_HOST, { useNewUrlParser: true })
@@ -18,36 +19,35 @@ mongoose
         name: "Admin User",
         email: "admin@example.com",
         password: await bcrypt.hash("password", 8),
-        role: 'admin',
+        role: "admin",
       });
 
       newAdmin
         .save()
-        .then(res =>
+        .then((res) =>
           console.log("Created default admin (admin@example.com; password)")
         )
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     }
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-
-var apiRouter = require('./routes/api/apiRouter');
+var apiRouter = require("./routes/api/apiRouter");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static('client/build'))
+app.use(express.static("client/build"));
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
